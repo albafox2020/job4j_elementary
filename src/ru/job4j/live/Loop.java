f6e4f13356d818a1f06962e14c1f922a28945117
Albina <albafox@yandex.ru>
2021-02-21 00:04:35 +0300

    5.9.1. Генерация конструкций в IDEA [#472753]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,9 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < ; i++) {
+            System.out.printf(" ");
+        }
+    }
+}
