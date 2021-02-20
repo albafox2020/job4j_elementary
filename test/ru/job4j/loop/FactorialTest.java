7dc3604cdd6c6f61a98d7e6464abe0ca04456fc0
Albina <albafox@yandex.ru>
2021-02-20 22:10:37 +0300

    5.4. Протеиновая диета [#472746]

--- test/ru/job4j/loop/FactorialTest.java
+++ test/ru/job4j/loop/FactorialTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static ru.job4j.loop.Factorial.calc;
 
 public class FactorialTest {
     @Test
