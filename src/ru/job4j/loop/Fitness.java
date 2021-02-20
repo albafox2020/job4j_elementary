d53711b972a54fc02ba4876344609f39d1082400
Albina <albafox@yandex.ru>
2021-02-20 22:13:26 +0300

    5.4. Протеиновая диета [#472746]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,13 +3,14 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-            while (ivan < nik ) {
+            while (ivan < nik) {
                 ivan = ivan * 3;
                 nik = nik * 2;
                 month++;
             }
             return month;
     }
+
     public static void main(String[] args) {
         int month = calc(90,100);
         System.out.println(month);
