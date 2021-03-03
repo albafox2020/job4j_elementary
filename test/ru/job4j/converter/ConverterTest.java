ded318e8d28b149be0f98496af2ba1df91cd0b9f
Albina <albafox@yandex.ru>
2021-03-03 23:19:41 +0300

    3. Объединить два отсортированных массива

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -10,7 +10,7 @@ public class ConverterTest {
     public void whenConvert140RblThen2Euro() {
         int in = 140;
         int expected = 2;
-        int out = Converter.rubleToEuro(in);
+        int out = Converter.rubToEuro(in);
         Assert.assertEquals(expected, out);
     }
 
@@ -18,7 +18,7 @@ public class ConverterTest {
     public void whenConvert180RblThen3Dlr() {
         int in = 180;
         int expected = 3;
-        int out = Converter.rubleToDollar(in);
+        int out = Converter.rubToDollar(in);
         Assert.assertEquals(expected, out);
     }
 }
